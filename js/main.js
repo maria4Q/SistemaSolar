ee4bc2d21f05c1afee12f52ee9d45e8185acd444
Cristopher <cdtum@scl.edu.gt>
2022-06-06 16:28:21 -0600

    section finalizado

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function activar_desactivar() {
+    var x = document.getElementById("planet1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+p1.addEventListener('click' , activar_desactivar);
+
+function activar_desactivar2() {
+    var x = document.getElementById("planet2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+p2.addEventListener('click' , activar_desactivar2);
+
+function activar_desactivar3() {
+    var x = document.getElementById("planet3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+p3.addEventListener('click' , activar_desactivar3);
+
+function activar_desactivar4() {
+    var x = document.getElementById("planet4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+p4.addEventListener('click' , activar_desactivar4);
+
+function activar_desactivar5() {
+    var x = document.getElementById("planet5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+p5.addEventListener('click' , activar_desactivar5);
